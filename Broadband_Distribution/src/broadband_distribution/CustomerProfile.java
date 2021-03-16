0ceb618b8b36fce18799e976637dddc29ddb0bac
ayshagazi <ayshagazi2477@gmail.com>
2021-03-16 11:09:25 +0600

    added

--- Broadband_Distribution/src/broadband_distribution/CustomerProfile.java
+++ Broadband_Distribution/src/broadband_distribution/CustomerProfile.java
@@ -529,7 +529,7 @@ public class CustomerProfile extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new CustomerProfile().setVisible(true);
+               // new CustomerProfile().setVisible(true);
                 
                 
                 
